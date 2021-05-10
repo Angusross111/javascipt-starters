f533ce6c780219a9bdcfc72974f01e7a673d0c60
Angus Ross <angus.ross@swarmfarm.com>
2021-05-10 16:30:42 +1000

    Changed

--- webpack-typescript-react-router/src/App.tsx
+++ webpack-typescript-react-router/src/App.tsx
@@ -1,37 +1,12 @@
-import React, { useState } from "react";
-import { Routes, Route, Link, Outlet } from 'react-router-dom';
-
-function Layout() {
-  return (
-    <div>
-      <h1>Welcome to the app!</h1>
-      <nav>
-        <Link to="invoices">Invoices</Link> |{' '}
-        <Link to="dashboard">Dashboard</Link>
-      </nav>
-      <div className="content">
-        <Outlet />
-      </div>
-    </div>
-  );
-}
-
-function Invoices() {
-  return <h1>Invoices</h1>;
-}
-
-function Dashboard() {
-  return <h1>Dashboard</h1>;
-}
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import RoutePaths from "./Routes";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route path="invoices" element={<Invoices />} />
-        <Route path="dashboard" element={<Dashboard />} />
-      </Route>
-    </Routes>
+   <Router>
+     <RoutePaths />
+   </Router>
   );
 }
 
---
+++ webpack-typescript-react-router/src/Routes.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { Routes, Route, Link, Outlet } from 'react-router-dom';
+
+function Invoices() {
+    return <h1>Invoices</h1>;
+  }
+  
+  function Dashboard() {
+    return <h1>Dashboard</h1>;
+  }
+
+  function Layout() {
+    return (
+      <div>
+        <h1>Welcome to the app!</h1>
+        <nav>
+          <Link to="invoices">Invoices</Link> |{' '}
+          <Link to="dashboard">Dashboard</Link>
+        </nav>
+        <div className="content">
+          <Outlet />
+        </div>
+      </div>
+    );
+  }
+
+  
+  function RoutePaths() {
+    return (
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="invoices" element={<Invoices />} />
+          <Route path="dashboard" element={<Dashboard />} />
+        </Route>
+      </Routes>
+    );
+  }
+
+  export default RoutePaths
